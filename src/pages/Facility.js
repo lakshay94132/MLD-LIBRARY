b10925f9f32a36a3d16ffdf704886e2c0e3440c3
lakshay94132 <lakshaylimba31@gmail.com>
2025-07-16 18:53:05 +0530

    home page fixing

--- src/pages/Facility.js
+++ src/pages/Facility.js
@@ -3,7 +3,32 @@ import React from 'react';
 export default function Facility() {
   return (
     <main>
-      <h2>Facility Page</h2>
+      <section className="my-4">
+  <h3>Our Library Facilities</h3>
+  <div className="row">
+    <div className="col-md-4 mb-3">
+      <img src="images/reading-zone.jpg" className="img-fluid rounded" alt="Students reading in library" />
+      <h5 className="mt-2">Peaceful Reading Zone</h5>
+    </div>
+    <div className="col-md-4 mb-3">
+      <img src="images/parking-area.jpg" className="img-fluid rounded" alt="Library parking area" />
+      <h5 className="mt-2">Spacious Parking</h5>
+    </div>
+    <div className="col-md-4 mb-3">
+      <img src="images/water-cooler.jpg" className="img-fluid rounded" alt="Library water cooler" />
+      <h5 className="mt-2">Clean Drinking Water</h5>
+    </div>
+    <div className="col-md-4 mb-3">
+      <img src="images/resting-area.jpg" className="img-fluid rounded" alt="Library resting section" />
+      <h5 className="mt-2">Resting & Relax Area</h5>
+    </div>
+    <div className="col-md-4 mb-3">
+      <img src="images/computer-zone.jpg" className="img-fluid rounded" alt="Library computer section" />
+      <h5 className="mt-2">Digital Reading Zone</h5>
+    </div>
+  </div>
+</section>
+
     </main>
   );
 }
