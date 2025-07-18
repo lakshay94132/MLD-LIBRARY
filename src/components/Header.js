0ad0b729abce59266ad7418a5039392118597919
lakshay94132 <lakshaylimba31@gmail.com>
2025-07-18 08:56:52 +0530

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({ mode, toggleMode }) {
     <header
       className={`d-flex justify-content-between align-items-center container my-1 py-3 rounded shadow mb-1 border border-2 `}
       style={{ 
-        fontSize: '12px',
+        fontSize: '11px',
         padding: '10px',
         lineHeight: '1.2',
         flexDirection: 'row',
@@ -18,9 +18,9 @@ export default function Header({ mode, toggleMode }) {
       }}
     >
 
-      <img src="/LOGO.png" alt="MLD Library Logo" style={{ height: '70px', width: '150px' }} />
+      <img src="/assets/LOGO.png" alt="MLD Library Logo" style={{ height: '70px', width: '150px' }} />
       <h1 className="m-0 text-center flex-grow-1"><strong>𝑀𝒜 𝐿𝐼𝒞𝐻𝐻𝑀𝒜 𝒟𝐸𝒱𝐼 𝐿𝐼𝐵𝑅𝒜𝑅𝒴</strong></h1>
-      <img src="/LOGO.png" alt="MLD Library Logo"  style={{ height: '70px', width: '150px' }}/>
+      <img src="/assets/LOGO.png" alt="MLD Library Logo"  style={{ height: '70px', width: '150px' }}/>
     </header>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,35 +3,28 @@ import React from 'react';
 export default function Home() {
   return (
     <main className="container my-4">
-      <h2>Welcome to MA LICHMA DEVI LIBRARY</h2>
-      <p>A place to read, learn, and grow.</p>
+       <section className="row">
 
-      <section className="my-4">
-        <h3>About Us</h3>
-        <p>
-          MLD LIBRARY is dedicated to providing students and the local community with access to thousands of books, research materials, and a peaceful reading environment. Established in 2025, the library continues to promote learning and knowledge for everyone.
-        </p>
-      </section>
+        
+      <img src='/assets/Testing.jpg' class="rounded float-start" alt="Back Ground Image"
+      style={{
+          height: '300px',
+          width: '300px',
+          marginLeft: 'auto',
+          marginBottom: 'auto',
+          marginTop: '20px'
+        }}
+/>
 
-      <section className="my-4">
-        <h3>Our Facilities</h3>
-        <ul>
-          <li>Large book collection</li>
-          <li>Digital reading zone</li>
-          <li>Comfortable reading spaces</li>
-          <li>Reference & research support</li>
-          <li>Free Wi-Fi</li>
-        </ul>
-      </section>
+      <img src="/assets/Testing.jpg" class="rounded float-end" alt="Back Ground Image"
+      style={{
+          height: '300px',
+          width: '300px',
+          marginLeft: 'auto',
+          marginBottom: 'auto',
+          marginTop: '20px'
+        }}/>
 
-      <section className="my-4">
-        <h3>Library Hours</h3>
-        <p>Mon – Sat: 9 AM – 6 PM<br />Sunday: Closed</p>
-      </section>
-
-      <section className="my-4">
-        <h3>Contact Us</h3>
-        <p>Visit Us: MLD LIBRARY, [Your Address Here]<br />Phone: [Your number]<br />Email: [Your email]</p>
       </section>
     </main>
   );
